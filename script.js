06c6bb069b4a7d8ef484578f6917cedc3baf10fd
ajstephens1995 <ajstephens1995@gmail.com>
2022-08-26 21:45:58 -0400

    Delete all the old javascript and add a Username and password prompt

--- script.js
+++ script.js
@@ -1,57 +1,17 @@
-const string = 'The revolution will not be televised.';
-console.log(string);
+let username = prompt("Enter Username");
 
-const badString = string;
-console.log(badString);
+if (username == "Admin") {
+  let password = prompt("Enter Password");
 
-const bigmouth= 'I\'ve got no right to take my place...';
-console.log(bigmouth);
-
-const visitorName = 'Chris';
-const greeting = `Hello, ${visitorName}`;
-console.log(greeting);
-
-const one = 'Hello, ';
-const two = 'how are you?';
-const joined = `${one}${two}`;
-console.log(joined);
-
-const button = document.querySelector('button');
-
-function greet() {
-    const name = prompt('What is your name?');
-    alert(`Hello ${name}, nice to see you!`);
+  if (password === "TheMaster") {
+    alert("Welcome!");
+  } else if (password === "" || password === null) {
+    alert("Canceled");
+  } else {
+    alert("Wrong Password");
+  }
+} else if (username === "" || username === null) {
+  alert("Canceled");
+} else {
+  alert("No Account");
 }
-
-button.addEventListener('click', greet);
-
-const greetings = 'Hello';
-const testName = 'Chris';
-
-console.log(`${greetings}, ${testName}`);
-
-const myString = '123';
-const myNum = Number(myString);
-console.log(typeof myNum);
-
-const myNum2 = 123;
-const myString2 = myNum2.toString();
-console.log(typeof myString2);
-
-const song = 'Fight the Youth';
-const score = 9;
-const highestScore = 10;
-const output = `I like the song ${song}. I gave it a score of ${score/highestScore * 100}%.`;
-console.log(output)
-
-let txt = '123456789';
-let length = txt.length;
-console.log(length)
-
-let str = 'Apple, Banana, Kiwi';
-let part = str.substr(7, 6);
-console.log(part)
-
-let text = 'Please visit Microsoft and Microsoft!';
-let newText = text.replace(/Microsoft/g, 'W3Schools');
-console.log(newText)
